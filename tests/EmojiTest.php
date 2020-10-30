964e2ba26bd936e292154adf6bb9b398c2844677
mauretto78 <maurocassani1978@gmail.com>
2020-10-30 17:07:17 +0100

    Emoji

---
+++ tests/EmojiTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Matecat\XliffParser\Tests;
+
+use Matecat\XliffParser\Utils\Emoji;
+
+class EmojiTest extends BaseTest
+{
+    /**
+     * @test
+     */
+    public function canReplaceEmojisWithEntites()
+    {
+        $dataset = [
+            '🤙 Join this (video)call at: {{joinUrl}}' => '&#129305; Join this (video)call at: {{joinUrl}}',
+            'Look 😀 It works! 🐻🌻' => 'Look &#128512; It works! &#128059;&#127803;',
+            '🗔' => '&#128468;',
+            '👨' => '&#128104;',
+            '🇺🇸' => '&#127482;&#127480;',
+            '9️⃣' => '&#57;&#65039;&#8419;',
+            '👋🏻' => '&#128075;&#127995;',
+        ];
+
+        foreach ($dataset as $emoji => $entity) {
+            $this->assertEquals($entity, Emoji::toEntity($emoji));
+        }
+    }
+}
