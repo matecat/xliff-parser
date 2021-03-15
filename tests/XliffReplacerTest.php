622ead9ccce3cb4a0512f41cf11240c4ba384068
Mauro Cassani <m.cassani@bestnetwork.it>
2021-03-15 16:51:00 +0100

    Revert to v1.0.45

--- tests/XliffReplacerTest.php
+++ tests/XliffReplacerTest.php
@@ -235,7 +235,7 @@ class RealXliffReplacerCallback implements XliffReplacerCallbackInterface
     /**
      * @inheritDoc
      */
-    public function thereAreErrors($segment, $translation)
+    public function thereAreErrors($segment, $translation, array $dataRefMap = [])
     {
         return false;
     }
@@ -246,7 +246,7 @@ class DummyXliffReplacerCallback implements XliffReplacerCallbackInterface
     /**
      * @inheritDoc
      */
-    public function thereAreErrors($segment, $translation)
+    public function thereAreErrors($segment, $translation, array $dataRefMap = [])
     {
         return false;
     }
@@ -257,7 +257,7 @@ class DummyXliffReplacerCallbackWhichReturnTrue implements XliffReplacerCallback
     /**
      * @inheritDoc
      */
-    public function thereAreErrors($segment, $translation)
+    public function thereAreErrors($segment, $translation, array $dataRefMap = [])
     {
         return true;
     }
