5fbdb5a109aeb5431ca3eb173a8dca0729f66550
Mauro Cassani <m.cassani@bestnetwork.it>
2021-04-28 15:48:57 +0200

    Placeholder fix

--- src/Constants/Placeholder.php
+++ src/Constants/Placeholder.php
@@ -4,7 +4,12 @@ namespace Matecat\XliffParser\Constants;
 
 class Placeholder
 {
-    const WHITE_SPACE_PLACEHOLDER = '###___WHITE_SPACE_TAB_PLACEHOLDER___###';
+    /**
+     * Placeholder map to preserve white spaces
+     * contained in <original-data> map
+     * (only for Xliff 2.0)
+     */
+    const WHITE_SPACE_PLACEHOLDER = '###___WHITE_SPACE_PLACEHOLDER___###';
     const NEW_LINE_PLACEHOLDER = '###___NEW_LINE_PLACEHOLDER___###';
     const TAB_PLACEHOLDER = '###___TAB_PLACEHOLDER___###';
 }
