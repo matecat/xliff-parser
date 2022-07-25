8824fb1bd82d46c9fb28518c44c23775392e811e
Mauro Cassani <maurocassani1978@gmail.com>
2022-07-25 18:15:39 +0200

    Fix emoji  '&#129717;'

--- tests/EmojiTest.php
+++ tests/EmojiTest.php
@@ -36,6 +36,7 @@ class EmojiTest extends BaseTest
         $dataset = [
             '🤙 Join this (video)call at: {{joinUrl}}' => '&#129305; Join this (video)call at: {{joinUrl}}',
             'Look 😀 It works! 🐻🌻' => 'Look &#128512; It works! &#128059;&#127803;',
+            '🪵'  => '&#129717;',
             '􀄿' => '&#1048895;',
             '🗔' => '&#128468;',
             '👨' => '&#128104;',
